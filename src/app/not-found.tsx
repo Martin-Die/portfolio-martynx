cf73694b75a42328c3449691338dd8405e809e5f
Martin-Die <martin.die.7559@gmail.com>
2024-12-09 13:05:41 +0100

    page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import PageNotFound from '../pages/404/pageNotFound';
+
+export default function NotFound() {
+    return <PageNotFound />;
+}
---
+++ src/pages/404/pageNotFound.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import './pageNotFound.scss';
+
+const PageNotFound = () => {
+
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back(); // Retour à la page précédente dans l'historique
+  };
+
+  return (
+    <div className="page-not-found">
+      <h1>404</h1>
+      <img
+        src="/images/shadu.png"
+        alt="Image not found"
+        style={{ width: '250px', marginBottom: '30px' }}
+      />
+      <p>How did you get there</p>
+      <a onClick={handleGoBack}>You should go back</a>
+    </div>
+  );
+};
+
+export default PageNotFound;
