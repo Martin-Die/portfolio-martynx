ed78339fdade17e1efd021dcc5f28f5011d43d35
Martin-Die <martin.die.7559@gmail.com>
2024-12-09 12:58:33 +0100

    presentation

--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -1,23 +1,40 @@
+'use client'
+
 import Image from 'next/image';
+import { useState } from "react";
 
 const Presentation = () => {
+
+    const [hovered, setHovered] = useState(false);
+
     return (
         <div className="hero-container">
-            <Image src='/images/niku.png' className="profile-img" width={300} height={300} alt="Mon portrait" />
+            <Image src='/images/niku.png' className="profile-img" width={300} height={300} alt="image temporaire" priority={true} />
             <div className="hero-text">
-                <h1>Bonjour, je suis [Votre Nom] 👋</h1>
+                <h1>Bonjour, je suis MartYnX</h1>
                 <p>
-                    Je suis un développeur basé à [Votre Ville]. Je me spécialise dans la création de sites web et d'applications exceptionnels.
+                    Je suis un développeur furry français. Je me spécialise dans la créations d'outils basé sur l'IA et bien plus.
                 </p>
                 <div className="social-icons">
-                    <a href="https://twitter.com/votreprofil" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
+                    <a href="https://x.com/DieMartin" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
                         <i className="fa-brands fa-twitter"></i>
                     </a>
-                    <a href="https://github.com/votreprofil" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
+                    <a
+                        href="https://fontawesome.com/icons"
+                        aria-label="Icon folder"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onMouseEnter={() => setHovered(true)}
+                        onMouseLeave={() => setHovered(false)}
+                    >
+                        <i className={hovered ? "fa-solid fa-folder-open" : "fa-solid fa-folder"}></i>
+                    </a>
+                    <a href="https://github.com/MartYnX" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
                         <i className="fa-brands fa-github"></i>
                     </a>
-                    <a href="https://www.linkedin.com/in/votreprofil/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
-                        <i className="fa-brands fa-linkedin"></i>
+                    <a
+                        href="https://bsky.app/profile/martynx.bsky.social" aria-label="bLUESKY" target="_blank" rel="noopener noreferrer">
+                        <i className="fa-brands fa-bluesky"></i>
                     </a>
                 </div>
             </div>
